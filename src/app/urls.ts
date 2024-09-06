c1d7ec1dabb379d7c4d04a3e93604e9d7654ce06
Rifky Ariya <78594605+rif42@users.noreply.github.com>
2024-09-06 09:18:12 +0700

    feat: add youtube link on notice page

--- src/app/urls.ts
+++ src/app/urls.ts
@@ -4,4 +4,5 @@ export const URL_AXIE_FORM =
 export const URL_DISCORD = "https://discord.gg/Am7ZmeZW"
 export const URL_TWITTER = "https://twitter.com/MetaPals"
 export const URL_EXTENSION_STORE = "https://metapals.ai/adopt"
+export const URL_YOUTUBE_METAPALS = "https://www.youtube.com/watch?v=w1ZHLx1WhB4"
 
