756baea94831799b4cab2493592cc5a532bb3a91
Rifky Ariya <78594605+rif42@users.noreply.github.com>
2024-09-09 10:52:39 +0700

    chore: remove accidental import

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -11,7 +11,6 @@ import { useState } from "react"
 import { AxieSendForm } from "../components/api/AxieSendForm"
 import { useRouter } from "next/navigation"
 import { URL_DISCORD, URL_TWITTER } from "../urls"
-import { serverHooks } from "next/dist/server/app-render/entry-base"
 
 interface inputChangeType {
   target: { name: string; value: string }
